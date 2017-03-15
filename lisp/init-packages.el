137d9ee34c9c4fa1ad9aa1b5f7baccf62115000f
zhangyupeng <zhangyupeng@parkingwang.com>
2017-03-15 14:01:37 +0800

    Add + slime to window

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -33,6 +33,17 @@
                                ;; --- Themes ---
                                monokai-theme
                                evil
+                               evil-surround
+                               evil-visualstar
+                               evil-mc
+                               evil-mark-replace
+                               evil-escape
+                               evil-space
+                               evil-numbers
+                               evil-matchit
+                               evil-nerd-commenter
+                               general
+                               slime
                                ;; solarized-theme
                                ) "Default packages")
 
